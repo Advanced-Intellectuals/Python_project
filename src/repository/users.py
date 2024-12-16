235026506e56450fcdf1222208c8f1d3f7230834
TRSalikhov <trsalikhov@mai.education>
2024-12-16 20:54:39 +0300

    Added user get by login and adding user to DB

--- src/repository/users.py
+++ src/repository/users.py
@@ -15,3 +15,18 @@ class UserRepo():
 
             users = result.unique().scalars().all()
             return users
+
+    async def get_by_login(self, login: str):
+        async with self.__async_session() as session:
+            statement = select(User).options(
+                joinedload(User.watched_movies),
+                joinedload(User.scores)).where(User.login == login)
+            result = await session.execute(statement)
+
+            user = result.scalars().first()
+            return user
+
+    async def write(self, user: User):
+        async with self.__async_session() as session:
+            session.add(user)
+            await session.commit()
