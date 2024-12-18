81d45184d5a672029ef85b22750252783cbb13c1
TRSalikhov <trsalikhov@mai.education>
2024-12-18 23:00:58 +0300

    Added repository for backend api

---
+++ ML/repository.py
@@ -0,0 +1,27 @@
+from sqlalchemy import select, insert
+from sqlalchemy.orm import joinedload
+from models import Score, User, watched_table
+from db import async_session
+
+
+class Repository():
+    __async_session = async_session
+
+    async def get_all(self):
+        async with self.__async_session() as session:
+            statement = select(Score)
+
+            res = await session.execute(statement)
+            scores = res.scalars().all()
+
+            return [{"user_id": s.user_id, "movie_id": s.movie_id, "score": s.score} for s in scores]
+
+    async def get_watched(self, user_id: int):
+        async with self.__async_session() as session:
+            statement = select(User).options(joinedload(
+                User.watched_movies), joinedload(User.scores)).where(User.user_id == user_id)
+
+            res = await session.execute(statement)
+            watched = res.scalars().first().watched_movies
+
+            return [w.movie_id for w in watched]
