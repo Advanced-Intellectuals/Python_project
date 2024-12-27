d6de1dde763613fd0145a0cb7ac0de75a924d5b4
Tim Salikhov <tim.dragonoid@gmail.com>
2024-12-27 06:57:20 +0300

    Frontend complete

--- frontend/pages/user/search.py
+++ frontend/pages/user/search.py
@@ -3,7 +3,14 @@ import os
 from util import draw_movies
 
 
-def find():
+def main():
+    st.session_state.search_url = f"{os.getenv('BACK_URL')}/search"
+
+    search_bar_column, search_button_column = st.columns(
+        [4, 1], vertical_alignment="bottom")
+
+    with search_bar_column:
+        st.session_state.search_bar = st.text_input("Введите название фильма:")
     if (st.session_state.search_bar):
         try:
             response = st.session_state.session.get(
@@ -15,15 +22,3 @@ def find():
                 st.error("Неправильные параметры.")
         except Exception as e:
             st.error(f"Ошибка подключения: {e}")
-
-
-def main():
-    st.session_state.search_url = f"{os.getenv('BACK_URL')}/search"
-
-    search_bar_column, search_button_column = st.columns(
-        [4, 1], vertical_alignment="bottom")
-
-    with search_bar_column:
-        st.session_state.search_bar = st.text_input("Введите название фильма:")
-    with search_button_column:
-        st.button('Найти', on_click=find)
