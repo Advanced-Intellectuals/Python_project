8e10fbd18b64dbb282e6b56bd896e33b645a2faf
Tim Salikhov <tim.dragonoid@gmail.com>
2024-12-27 08:10:47 +0300

    Fixed ssl on backedn and ml

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,5 @@
 from composition_root import CompositionRoot
 import uvicorn
-import ssl
 import os
 from dotenv import load_dotenv
 
@@ -10,11 +9,6 @@ cr = CompositionRoot()
 
 app = cr.get_app()
 
-ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-ssl_context.load_cert_chain(
-    os.getenv("CERT_PATH"),
-    keyfile=os.getenv("KEY_PATH")
-)
-
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=8000, ssl=ssl_context)
+    uvicorn.run(app, host='0.0.0.0', port=8000,
+                ssl_certfile=os.getenv("CERT_PATH"), ssl_keyfile=os.getenv("KEY_PATH"))
